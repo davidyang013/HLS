2c8fb740f0eac53de29887d5a9c92b4f417aaa8f
david yang <davidyang013@gmail.com>
2015-01-21 16:21:57 +0800

    Update send.py
    Update it

--- send.py
+++ send.py
@@ -1,15 +1,16 @@
-'''
-Created on 2013-2-22
+__author__ = 'xweiyan'
 
-@author: xweiyan
-'''
+
 from hls import load
 import hls
 import os
+import re
 import shutil
 import string
 import tempfile
 import time
+import threading
+import sys
 '''
 SCAN the segments folder
 '''
@@ -17,6 +18,24 @@ SCAN the segments folder
 '''
 scan the segments folder, return a list
 '''
+
+'''
+counter
+'''
+class Counter:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.value = 0
+
+    def increment(self):
+        self.lock.acquire() #critical section
+        self.value = value = self.value + 1
+        self.lock.release()
+        return value
+
+counter = Counter()
+
+
 def scan(path):
     _segments = {}
     files = os.listdir(path)
@@ -32,74 +51,84 @@ def txt2dict(file):
     data = hls.M3U8(content)
     for no, segment in enumerate(data.segments):
         segments[no] = str(segment)
-    return segments    
- 
+    return segments
+
 def string_to_lines(string):
-    return string.strip().replace('\r\n', '\n').split('\n')   
-'''
-#EXTM3U
-#EXT-X-VERSION:3
-#EXT-X-TARGETDURATION:11
-#EXT-X-MEDIA-SEQUENCE:56
-#EXTINF:10,
-20130221T134839-01-56.ts
-#EXTINF:10,
-20130221T134839-01-57.ts
-#EXTINF:10,
-20130221T134839-01-58.ts
-#EXTINF:10,
-20130221T134839-01-59.ts
-#EXTINF:10,
-20130221T134839-01-60.ts
-#EXTINF:10,
-20130221T134839-01-61.ts
-'''  
-def playlist(pl_uri, segments):
+    return string.strip().replace('\r\n', '\n').split('\n')
+
+def getSequence(segment,prefix):
+    pattern = "(\w{15})-(\d{2})-(\d+).ts"
+    sep = "\n"
+    for line in str(segment).split(sep):
+        if line.__contains__("http") or  line.__contains__("https"):
+            line = line[len(prefix):]
+            m = re.match(pattern,line)
+            if m is not None:
+                return int(m.group(3))
+        else:
+            m = re.match(pattern,line)
+            if m is not None:
+                return int(m.group(3))
+    return None
+
+
+
+def playlist(pl_uri, segments, index,prefix = None):
     #get the last playlist sequence index
-    sequence = load(pl_uri).media_sequence
+    _segments = load(pl_uri).segments
+    print _segments
+    sequence = getSequence(_segments,prefix)
+    print sequence
     playList = []
     if sequence + 6 <= len(segments) - 1:
         playList = segments.values()[sequence + 1 : sequence + 7]
+        # print playlist
     else:
-        playList = segments.values()[sequence - len(segments) + 1:] + segments.values()[:7 - len(segments) + sequence]
+        playList = segments.values()[sequence + 1:] + segments.values()[:7 - len(segments) + sequence]
     output = ['#EXTM3U', '#EXT-X-VERSION:3', '#EXT-X-TARGETDURATION:11']
-    if sequence + 1 <= len(segments):
-        output.append("#EXT-X-MEDIA-SEQUENCE:" + str(sequence + 1))
-    else:
-        output.append("#EXT-X-MEDIA-SEQUENCE:" + str(sequence + 1 - len(segments)))
-    for segment in playList:
-        output.append(segment)
-#    print output
-    return '\n'.join(output)
-    
-def generate(path, interval=10):
-    files = []
-    for file in os.listdir(path):
-        if os.path.splitext(file)[1] == ".txt":
-            files.append(file)
-    while True:
-        for file in files:
-            m3u8 = os.path.join(path, os.path.splitext(file)[0] + ".m3u8")
-            segments = txt2dict(os.path.join(path, file))
-            #create a tempple file
-            temp = tempfile.mktemp()
-#            print temp
-            file = open(temp, 'w')
-            output = playlist(m3u8, segments)
-#            print output
-            file.write(output)
-            file.close()
-            shutil.copy(temp, m3u8)
-            try:
-                os.remove(temp)
-            except OSError:
-                pass
-        time.sleep(interval)
 
+    output.append("#EXT-X-MEDIA-SEQUENCE:" + str(index))
+
+    for segment in playList:
+        if prefix is not None:
+            segment = wrapSegment(segment,prefix)
+        output.append(segment)
+    return '\n'.join(output)
+
+def wrapSegment(segment,url):
+    return segment.split("\n")[0] + "\n" + url + segment.split("\n")[1]
+
+class Generate(threading.Thread):
+    def __init__(self, path, interval=10,prefix=None):
+        threading.Thread.__init__(self)
+        self.path = path
+        self.interval = interval
+        self.prefix = prefix
+        self.lock = threading.Lock()
+        self.files = []
+        for file in os.listdir(path):
+            if os.path.splitext(file)[1] == ".txt":
+                self.files.append(file)
+    def run(self):
+        while True:
+            self.lock.acquire()
+            index = counter.increment()
+            for file in self.files:
+                m3u8 = os.path.join(self.path, os.path.splitext(file)[0] + ".m3u8")
+                segments = txt2dict(os.path.join(self.path, file))
+                temp = tempfile.mktemp()
+                file = open(temp, 'w')
+                output = playlist(m3u8, segments, index, self.prefix)
+                file.write(output)
+                file.close()
+                shutil.copy(temp, m3u8)
+                try:
+                    os.remove(temp)
+                except OSError:
+                    pass
+            time.sleep(self.interval)
+            self.lock.release()
 
 if __name__ == "__main__":
-#    print scan('c:\\Users\\xweiyan\\Desktop\\1')
-#    segments = generate(r'C:\development\Apache Software Foundation\Apache2.2\uploads\1')
-#    print segments
-    
-    generate(r"C:\stream\5")
+    gen = Generate(path = r"/Users/david/Downloads/1",interval=10,prefix = "http://10.170.78.254/webdav/content/1/")
+    gen.start()
